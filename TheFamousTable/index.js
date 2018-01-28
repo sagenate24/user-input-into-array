43c2d50b1db2777f9f65a28b677379f79e03dcf0
Alex Sage <sagezander@gmail.com>
2018-01-28 12:49:41 -0800

    Added the famous table example

---
+++ TheFamousTable/index.js
@@ -0,0 +1,50 @@
+var famousPeople = [
+    {
+      firstName: 'Oprah',
+      lastName: 'Winfrey',
+      sex: 'female',
+      age: 63
+    },
+    {
+      firstName: 'Leonardo',
+      lastName: 'DiCaprio',
+      sex: 'male',
+      age: 43
+    },
+    {
+      firstName: 'Angelina',
+      lastName: 'Jolie',
+      sex: 'female',
+      age: 42
+    },
+    {
+      firstName: 'Joe',
+      lastName: 'Biden',
+      sex: 'male',
+      age: 75
+    },
+    {
+      firstName: 'Jennifer',
+      lastName: 'Lawrence',
+      sex: 'female',
+      age: 27
+    },
+    {
+      firstName: 'Morgan',
+      lastName: 'Freeman',
+      sex: 'male',
+      age: 80
+    },
+    {
+      firstName: 'Chriss',
+      lastName: 'Pratt',
+      sex: 'male',
+      age: 38
+    },
+    {
+      firstName: 'Scarlett',
+      lastName: 'Johansson',
+      sex: 'female',
+      age: 33
+    }
+];
